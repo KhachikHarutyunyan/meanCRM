1664796f64e21098ffe78c8d427a900f816c3630
KhachikHarutyunyan <khachik789@gmail.com>
2018-07-09 17:38:44 +0400

    history page

--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
+import { MaterialInstance, MaterialService } from '../shared/classes/material.service';
+import { OrdersService } from '../shared/services/orders.service';
+import { Subscription } from 'rxjs';
+import { Order, Filter } from '../shared/interfaces';
+
+const STEP = 2;
 
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.css']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('tooltip') tooltipRef: ElementRef;
+  tooltip: MaterialInstance;
+  isFilterVisible = false;
+  onSub: Subscription;
+  orders: Order[] = [];
+  loading = false;
+  reloading = false;
+
+  offset = 0;
+  limit = STEP;
+  noMoreOrders = false;
+  filter: Filter = {};
+
+  constructor(
+    private ordersService: OrdersService
+  ) { }
 
   ngOnInit() {
+    this.reloading = true;
+    this.fetch();
+  }
+
+  private fetch() {
+    const params = Object.assign({}, this.filter, {
+      offset: this.offset,
+      limit: this.limit
+    });
+
+    this.onSub = this.ordersService.fetch(params).subscribe(orders => {
+      this.orders = this.orders.concat(orders);
+      this.noMoreOrders = orders.length < STEP;
+      this.loading = false;
+      this.reloading = false;
+    });
+  }
+
+  applyFilter(filter: Filter) {
+    this.orders = [];
+    this.offset = 0;
+    this.filter = filter;
+    this.reloading = true;
+    this.fetch();
+  }
+
+  isFiltered(): boolean {
+    return Object.keys(this.filter).length !== 0;
+  }
+
+  ngAfterViewInit() {
+    this.tooltip = MaterialService.initTooltip(this.tooltipRef);
+  }
+
+  loadMore() {
+    this.offset += STEP;
+    this.loading = true;
+    this.fetch();
+  }
+
+  ngOnDestroy() {
+    this.tooltip.destroy();
+    this.onSub.unsubscribe();
   }
 
 }
--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,7 +1,10 @@
-import { OrderService } from './../shared/services/order.service';
+import { OrderService } from './order.service';
 import { MaterialService, MaterialInstance } from './../shared/classes/material.service';
 import { Router, NavigationEnd } from '@angular/router';
 import { Component, OnInit, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
+import { OrderPosition, Order } from '../shared/interfaces';
+import { OrdersService } from '../shared/services/orders.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-order-page',
@@ -14,10 +17,13 @@ export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
   isRoot: boolean;
   @ViewChild('modal') modalRef: ElementRef;
   modal: MaterialInstance;
+  pending = false;
+  oSub: Subscription;
 
   constructor(
     private router: Router,
-    private orderService: OrderService
+    public order: OrderService,
+    private ordersService: OrdersService
   ) { }
 
   ngOnInit() {
@@ -38,11 +44,35 @@ export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   submit() {
-    this.modal.close();
+    this.pending = true;
+    const order: Order = {
+      list: this.order.list.map(item => {
+        delete item._id;
+        return item;
+      })
+    };
+    this.oSub = this.ordersService.create(order).subscribe(
+      newOrder => {
+        MaterialService.toast(`Заказ N${newOrder.order} был добавлен.`);
+        this.order.clear();
+      },
+      err => MaterialService.toast(err.error.message),
+      () => {
+        this.pending = false;
+        this.modal.close();
+      }
+    );
+  }
+
+  deletePosition(orderPosition: OrderPosition) {
+    this.order.remove(orderPosition);
   }
 
   ngOnDestroy() {
     this.modal.destroy();
+    if (this.oSub) {
+      this.oSub.unsubscribe();
+    }
   }
 
 
